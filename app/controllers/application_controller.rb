3cfd6a595484ab3bae0dba25548c8cc2944ee5be
xemnikk <xemnikk@gmail.com>
2020-06-23 16:39:11 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,21 @@ class ApplicationController < Sinatra::Base
 
     set(:views, 'app/views')
     
+    get('/list_airlines') do 
+        @airlines = Airline.all
+        erb(:list_airlines)
+    end
 
+    get('/get-airline/:id') do
+        @airline = Airline.find(params[:id])
+        erb :single_airline
+    end
+
+    post("/destroy/:id") do
+        @airline = Airline.find(params[:id])
+        @airline.destroy()
+    end
 end
-
 
 ### HTTP Method  + URL  -> Address/Function Name
 ### Headers -> Postage
